95a826628075d8f5d09a311c2468ca05bfd667b1
jubika04 <69981300+jubika04@users.noreply.github.com>
2020-11-04 13:45:08 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,20 @@
+class Box
+{
+    constructor(x,y,width,height)
+    {
+        var options={
+            restitution : 0.8
+        }
+
+        this.object = Bodies.rectangle(x,y,width,height,options);
+        World.add(world,this.object);
+    }
+
+    display()
+    {
+        rectMode(CENTER);
+        rect(this.object.position.x, this.object.position.y,this.width,this.height);
+    }
+
+
+}
